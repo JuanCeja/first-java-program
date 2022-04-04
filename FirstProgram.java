6628fe5e17174d8e18015891dbc5a6a98eeeedae
Juan Ceja <jmc347ls2@yahoo.com>
2022-04-04 13:17:59 -0700

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Juan Ceja");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Delano,CA");
+    }
+}
